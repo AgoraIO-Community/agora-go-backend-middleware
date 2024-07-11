29b4c191a44b83593b0e5f1e7966ae04f66e03e2
digitallysavvy <hermes.frangoudis@gmail.com>
2024-07-11 16:15:48 -0400

    add rtmp utils

---
+++ rtmp_service/rtmp_utils.go
@@ -0,0 +1,46 @@
+package rtmp_service
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+// generateUID generates a unique user identifier for use within cloud recording sessions.
+// This function ensures the UID is never zero, which is reserved, by generating a random
+// number between 1 and the maximum possible 32-bit integer value.
+func (s *RtmpService) GenerateUID() string {
+	// Generate a random number starting from 1 to avoid 0, which is reserved.
+	uid := rand.Intn(4294967294) + 1
+
+	// Convert the integer UID to a string format and return it.
+	return strconv.Itoa(uid)
+}
+
+// ValidateRecordingMode checks if a specific string is present within a slice of strings.
+// This is useful for determining if a particular item exists within a list.
+func (s *RtmpService) ValidateRecordingMode(valideModes []string, modeToCheck string) bool {
+	for _, mode := range valideModes {
+		if mode == modeToCheck {
+			return true
+		}
+	}
+	return false
+}
+
+// AddTimestamp adds a current timestamp to any response object that supports the Timestampable interface.
+// It then marshals the updated object back into JSON format for further use or storage.
+func (s *RtmpService) AddTimestamp(response Timestampable) (json.RawMessage, error) {
+	// Set the current timestamp in UTC and RFC3339 format.
+	now := time.Now().UTC().Format(time.RFC3339)
+	response.SetTimestamp(now)
+
+	// Marshal the response with the added timestamp back to JSON.
+	timestampedBody, err := json.Marshal(response)
+	if err != nil {
+		return nil, fmt.Errorf("error marshaling final response with timestamp: %v", err)
+	}
+	return timestampedBody, nil
+}
