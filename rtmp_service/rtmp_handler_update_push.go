4bc173437ca5309f0b59072dba2eede528163f8a
digitallysavvy <hermes.frangoudis@gmail.com>
2024-07-12 16:27:35 -0400

    updated comments

--- rtmp_service/rtmp_handler_update_push.go
+++ rtmp_service/rtmp_handler_update_push.go
@@ -5,35 +5,37 @@ import (
 	"fmt"
 )
 
-// HandleStopPushReq stops an RTMP push request using Agora's Media Push service.
-// It constructs the request URL and sends the stop request to the Agora API using the makeRequest helper function.
+// HandleUpdatePushReq updates an existing RTMP push request using Agora's Media Push service.
+// It constructs the request URL and sends the update request to the Agora API using the makeRequest helper function.
 //
 // Parameters:
+//   - updateReq: RtmpPushRequest - Contains the configuration settings for the update RTMP request.
 //   - converterId: string - The ID of the Converter returned in the start push request.
-//   - region: string - The region ID previously acquired to identify the resource for the recording.
+//   - region: string - The region ID for the rtmp resource.
 //   - requestID: string - The unique request ID for tracing the request.
 //
 // Returns:
-//   - json.RawMessage: The raw JSON response indicating the status of the stop request.
+//   - json.RawMessage: The raw JSON response indicating the status of the update request.
 //   - error: Error object detailing any issues encountered during the API call.
 //
 // Behavior:
-//   - Constructs the URL for stopping the recording session based on the provided parameters.
-//   - Sends a DELETE request to the Agora endpoint to stop the recording.
+//   - Constructs the URL for updating the rtmp session based on the provided parameters.
+//   - Sends a PATCH request to the Agora endpoint to update the rtmp resource.
 //   - Creates a success response for the client as the successful response won't have a body.
 //   - Appends a timestamp to the response for record-keeping before returning the modified response.
 //
 // Notes:
-//   - Assumes the presence of s.baseURL for constructing the request URL.
+//   - Assumes the presence of s.baseURL and s.rtmpURL for constructing the request URL.
 //   - Utilizes s.makeRequest for sending the HTTP request and handling the response.
-func (s *RtmpService) HandleUpdatePushReq(converterId string, region string, requestID string) (json.RawMessage, error) {
-	// Construct the URL for the stop recording endpoint.
+//   - Uses s.AddTimestamp to append a timestamp to the response.
+func (s *RtmpService) HandleUpdatePushReq(updateReq RtmpPushRequest, converterId string, region string, requestID string) (json.RawMessage, error) {
+	// Construct the URL for the update rtmp endpoint.
 	url := fmt.Sprintf("%s/%s/%s/rtmp-converters/%s", s.baseURL, region, s.rtmpURL, converterId)
 
 	fmt.Println("HandleUpdatePushReq with url: ", url)
 
-	// Send a DELETE request to the stop recording endpoint.
-	_, err := s.makeRequest("DELETE", url, nil, requestID)
+	// Send a PATCH request to the update rtmp endpoint.
+	_, err := s.makeRequest("PATCH", url, updateReq, requestID)
 	if err != nil {
 		return nil, err
 	}
