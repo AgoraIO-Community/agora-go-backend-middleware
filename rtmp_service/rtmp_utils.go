d81241590c67b6263d9596a1a91818234ac4661a
digitallysavvy <hermes.frangoudis@gmail.com>
2024-07-12 16:03:31 -0400

    removed unused method

--- rtmp_service/rtmp_utils.go
+++ rtmp_service/rtmp_utils.go
@@ -3,22 +3,9 @@ package rtmp_service
 import (
 	"encoding/json"
 	"fmt"
-	"math/rand"
 	"net"
-	"strconv"
 	"time"
 )
-
-// generateUID generates a unique user identifier for use within cloud recording sessions.
-// This function ensures the UID is never zero, which is reserved, by generating a random
-// number between 1 and the maximum possible 32-bit integer value.
-func (s *RtmpService) GenerateUID() string {
-	// Generate a random number starting from 1 to avoid 0, which is reserved.
-	uid := rand.Intn(4294967294) + 1
-
-	// Convert the integer UID to a string format and return it.
-	return strconv.Itoa(uid)
-}
 
 // ValidateRegion checks if a specific string is present within a slice of strings.
 // This is useful for determining if a particular item exists within a list.
